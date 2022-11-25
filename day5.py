f4358bdb0b636ce6231436d3230a2f7480a20aa8
Kirankp <jnvkirankp25522@gmail.com>
2022-11-25 22:35:16 +0530

    update

--- day5.py
+++ day5.py
@@ -74,9 +74,111 @@ def dis2():
     for i in cur.execute("select name,phone from jspm"):
         n,contact=i
         print("name:",n,"\nphone:",contact)
-#main
-conn=sqlite3.connect("university.db")
-cur=conn.cursor()
-# create(cur)
-#insert()
-dis2()
+
+# def update():
+#     p=input("enter no:")
+#     secmd="UPDATE jspm SET phone = p"
+#     cur.execute(secmd)
+#     print("data updated successfully")
+#     conn.commit()
+#     dis2()
+# def insert2():
+#     li=[]
+#     name=input("enter name:")
+#     id=int(input("enter id:"))
+#     add=input("address:")
+#     ph=int(input("phone number:"))
+#     li.append(id)
+#     li.append(name)
+#     li.append(add)
+#     li.append(ph)
+#     selcmd="insert into jspm values(?,?,?,?)"
+#     cur.execute(selcmd)
+#     print("data inserted successfully")
+#     conn.commit()
+#     insert2()
+
+ 
+
+
+
+# #main
+# conn=sqlite3.connect("university.db")
+# cur=conn.cursor()
+# # create(cur)
+# #insert()
+# # dis2()
+# # update()
+# insert2()
+# display()
+####################################################################
+# import pandas as pd
+# data={'test':[1,2,3,4],
+#       'name':['a','b','c','d'],
+#       'fees':[100,200,300,400]}
+# df=pd.Dataframe(data)
+# print(df)
+# pyplot module is used to draw praph
+###################################################################################################
+# from tkinter import *
+# from tkinter import messagebox
+# def test():
+#     userid=t1.get()
+#     password=t2.get()
+#     if password=="kp25@123":
+#         messagebox.showinfo("login successfully")
+#     else:
+#         messagebox.showinfo("invalid password")
+# def reset_fun():
+#     t1.delete(0,END)
+#     t2.delete(0,END)
+#     t1.insert(0,"insert user id:")
+
+
+
+# root=Tk()
+# root.title("first GUI")
+# root.geometry("500x500")
+# l1=Label(text="welcome to jspm API",font=(14))
+# l1.grid(row=1,column=2,padx=5,pady=5)
+# l2=Label(text="enter name",font=(14))
+# l2.grid(row=2,column=1,padx=5,pady=5)
+
+# t1=Entry(font=(14))
+# t1.grid(row=2,column=2,padx=5,pady=5)
+# l3=Label(text="password:",font=(14))
+# l3.grid(row=3,column=1,padx=5,pady=5)
+# t2=Entry(font=(14),show="*")
+# t2.grid(row=3,column=2,padx=5,pady=5)
+# b1=Button(text="Submit:",command=test)
+# b1.grid(row=4,column=2,padx=5,pady=5)
+# root.mainloop()
+#####################################################################################
+## CALCULATOR ##
+from tkinter import *
+from tkinter import messagebox
+root=Tk()
+root.title("CALCULATOR USING PYTHON")
+root.geometry("500x500")
+l1=Label(text="CALCULATOR",font=(14))
+l1.grid(row=1,column=2,padx=5,pady=5)
+
+b1=Button(text="1")
+b1.grid(row=2,column=1,padx=5,pady=5)
+b2=Button(text="2")
+b2.grid(row=2,column=2,padx=5,pady=5)
+b3=Button(text="3")
+b3.grid(row=2,column=3,padx=5,pady=5)
+b4=Button(text="4")
+b4.grid(row=3,column=1,padx=5,pady=5)
+b5=Button(text="5")
+b5.grid(row=3,column=2,padx=5,pady=5)
+b6=Button(text="6")
+b6.grid(row=3,column=3,padx=5,pady=5)
+b7=Button(text="7")
+b7.grid(row=4,column=1,padx=5,pady=5)
+b8=Button(text="8")
+b8.grid(row=4,column=2,padx=5,pady=5)
+b9=Button(text="9")
+b9.grid(row=4,column=3,padx=5,pady=5)
+root.mainloop()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# root=Tk()
+# root.title("first gui")
+# root.geometry("400x500")
+# l1=Label(text="Welcome to BARCLAYS TRAINING",font=(24))
+# l1.grid(row=1,column=2)
+# l2=Label(text="student details",font=(14))
+# l2.grid(row=2,column=2)
+
+# t1=Entry(font=(14))
+# t1.grid(row=3,column=2,padx=8,pady=8)
